1c9b10c4f11283071564f462b49ee2af231e476a
Lucien XU <sfietkonstantin@free.fr>
2018-12-01 21:38:24 +0100

    test

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -17,6 +17,11 @@ fi
 
 if [[ "$TRAVIS_OS_NAME" == "windows" ]]
 then
-    curl -OL http://download.qt.io/official_releases/qt/5.11/5.11.2/qt-opensource-windows-x86-5.11.2.exe
-    ./qt-opensource-windows-x86-5.11.2.exe -h
+    if [[ ! -d /c/Qt ]]
+    then
+        curl -OL http://download.qt.io/official_releases/qt/5.11/5.11.2/qt-opensource-windows-x86-5.11.2.exe
+        ./qt-opensource-windows-x86-5.11.2.exe --script ci/install.qs
+    fi
+    ls /c/Qt/
+    ls /c/Qt/dist/
 fi
